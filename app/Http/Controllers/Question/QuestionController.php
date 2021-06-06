5bf707deca6b86b72f2e028877b5b3c96ee9707b
taaachezi <taaachezi@gmail.com>
2021-06-06 08:02:35 +0000

    question_controller cerate index作成

--- app/Http/Controllers/Question/QuestionController.php
+++ app/Http/Controllers/Question/QuestionController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers\Question;
 
+use App\Model\Questions\Question;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function create() {
+        return view("questions/create");
+    }
+
+    public function index(){
+        $questions = Question::all();
+        return view("questions/index");
+    }
 }
