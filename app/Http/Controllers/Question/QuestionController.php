98bd35ca4ee949b176f1f4ff0c48a381060dbaf6
taaachezi <taaachezi@gmail.com>
2021-06-12 09:23:37 +0000

    question_layout

--- app/Http/Controllers/Question/QuestionController.php
+++ app/Http/Controllers/Question/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Question;
 
 use App\Model\Questions\Question;
+use App\Model\Questions\QuestionCategory;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -12,13 +13,15 @@ class QuestionController extends Controller
         return view("questions/create");
     }
 
-    public function index(int $id){
+    public function index(){
         $questions = Question::all();
+        $question_category = new QuestionCategory();
+        $category = $question_category->category();
         // キーがviewページで変数名として使用できる
         // 引数でidを取得しcurrent表示する（current_question_id）としてviewに渡す
         return view("questions/index",[
             "questions" => $questions,
-            "current_question_id" => $id,
+            "category" => $category,
         ]);
     }
 }
