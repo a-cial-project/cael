44164348f432e95e6ab726a22d7622922f345ebf
taaachezi <taaachezi@gmail.com>
2021-06-30 14:27:21 +0000

    ckeditorエラー修正

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -19,9 +19,19 @@
       <form action="{{ route('question.store') }}" method="post" accept-charset="utf-8">
         @csrf
         <!-- cdeditorの生成　class命名による -->
-       <textarea class="ckeditor" id="editor" name="ckeditor"></textarea>
+       <textarea id="editor" name="ckeditor"></textarea>
         <input class="btn btn-primary" type="submit" name="question_submit" value="質問確認画面へ">
       </form>
+      <!-- scriptがページを読み込む前に発動するため、一定時間を置いて起動させる -->
+      <script>
+        setTimeout(function(){
+           CKEDITOR.replace("editor", {
+            uiColor: "#EEEEEE",
+            width:700,
+            height:600,
+          });
+        },);
+      </script>
     </div>
   </div>
 </div>
